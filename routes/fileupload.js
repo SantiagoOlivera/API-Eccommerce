8738de34a40c5aa887c728300addca8d4d85c192
SantiagoOlivera <olivera.santiagoariel@gmail.com>
2019-12-04 23:57:06 -0300

    api express upload images

--- routes/fileupload.js
+++ routes/fileupload.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const crypto = require('crypto');
 var router = express.Router();
 
 //require multer for the file uploads
@@ -6,15 +7,26 @@ var multer = require('multer');
 // set the directory for the uploads to the uploaded to
 var DIR = './public/img/product-images/';
 //define the type of upload multer would be doing and pass in its destination, in our case, its a single file with the name image
-var upload = multer({dest: DIR}).single('image');
-
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* var upload = multer(
+  { dest: DIR }
+  ).single('image'); */
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, DIR)
+    },
+    filename: function (req, file, cb) {
+      let id = crypto.randomBytes(18).toString('hex');
+      let ext = file.originalname.substring(file.originalname.lastIndexOf('.'), file.originalname.length);
+      cb(null, `${id}${ext}`);
+    }
 });
+
+var upload = multer({ storage: storage }).single('image'); 
 
 router.post('/add', function(req, res, next) {
     var path = '';
-     upload(req, res, function (err) {
+     upload(req, res, 
+       function (err) {
         if (err) {
           // An error occurred when uploading
           console.log(err);
@@ -26,3 +38,5 @@ router.post('/add', function(req, res, next) {
   });     
   
 });
+
+module.exports = router;
