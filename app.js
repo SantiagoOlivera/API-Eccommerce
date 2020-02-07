fda05ddbb6b44382b9f23c72ad4d144ef1593e4b
SantiagoOlivera <olivera.santiagoariel@gmail.com>
2020-02-07 18:25:36 -0300

    Cambios headers

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next){
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
 });
 app.options("/*", function(req, res, next){
